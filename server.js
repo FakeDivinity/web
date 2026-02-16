d2f6d8df3f293e7d0304981956142b3cc805fa7f
FakeDivinity <103278415+FakeDivinity@users.noreply.github.com>
2026-02-16 17:26:55 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
+const port = 3000;
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const expressApp = express();
+  const httpServer = createServer(expressApp);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: `http://${hostname}:${port}`, // Allow connections from the Next.js app
+      methods: ['GET', 'POST'],
+    },
+  });
+
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+      io.emit('chat message', msg);
+    });
+  });
+
+  expressApp.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  httpServer.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});
